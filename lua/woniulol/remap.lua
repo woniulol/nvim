31dfd99ba8e6bfdb0ad96275730c9a8096705082
jnwang <jnwangcn@gmail.com>
2025-12-05 11:11:45 +0800

    "feat: enable remap scrolloff."

--- lua/woniulol/remap.lua
+++ lua/woniulol/remap.lua
@@ -79,3 +79,5 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- Prevent cursor drop to bottom
+vim.o.scrolloff = 20
