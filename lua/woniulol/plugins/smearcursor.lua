f6df4d0ec74612c54fd02d862d6f6e7b20bc824b
jnwang <jnwangcn@gmail.com>
2025-12-09 10:15:27 +0800

    update: smear cursor use transparent bg.

--- lua/woniulol/plugins/smearcursor.lua
+++ lua/woniulol/plugins/smearcursor.lua
@@ -1,6 +1,9 @@
 return {
     {
         "sphamba/smear-cursor.nvim",
-        opts = {},
+        opts = {
+            legacy_computing_symbols_support = true,  -- Transparent background
+            time_interval = 5,
+        },
     },
 }
