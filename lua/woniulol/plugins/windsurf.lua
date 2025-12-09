a6ce8ea947d36217c86cdd107bf31b8497eec282
jnwang <jnwangcn@gmail.com>
2025-12-09 17:36:20 +0800

    feat: include windsurf

---
+++ lua/woniulol/plugins/windsurf.lua
@@ -0,0 +1,13 @@
+return {
+    -- Check https://github.com/Exafunction/windsurf.nvim for authentication.
+    {
+        "Exafunction/windsurf.nvim",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "hrsh7th/nvim-cmp",
+        },
+        config = function()
+            require("codeium").setup({})
+        end
+    },
+}
