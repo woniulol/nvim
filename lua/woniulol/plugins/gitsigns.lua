a0d57545a373c734b195a88a559e3cfc3eee5a5d
jnwang <jnwangcn@gmail.com>
2025-12-11 14:38:36 +0800

    update: update gitsigns keymap.

--- lua/woniulol/plugins/gitsigns.lua
+++ lua/woniulol/plugins/gitsigns.lua
@@ -1,15 +1,20 @@
 return {
     { -- Adds git related signs to the gutter, as well as utilities for managing changes
         'lewis6991/gitsigns.nvim',
-        opts = {
-            signs = {
-                add = { text = '+' },
-                change = { text = '~' },
-                delete = { text = '_' },
-                topdelete = { text = '‾' },
-                changedelete = { text = '~' },
-            },
-        },
+        config = function()
+            require('gitsigns').setup {
+                signs = {
+                    add = { text = '+' },
+                    change = { text = '~' },
+                    delete = { text = '_' },
+                    topdelete = { text = '‾' },
+                    changedelete = { text = '~' },
+                },
+            }
+            vim.keymap.set('n', '<leader>gph', ":Gitsigns preview_hunk<CR>", { desc = "Gitsigns [p]review [h]unk" })
+            vim.keymap.set('n', "<leader>gbl", ":Gitsigns blame_line<CR>", { desc = "Gitsigns [b]lame [l]ine" })
+            vim.keymap.set('n', "<leader>gbf", ":Gitsigns blame<CR>", { desc = "Gitsigns [b]lame [f]ile" })
+        end
     },
 }
 
