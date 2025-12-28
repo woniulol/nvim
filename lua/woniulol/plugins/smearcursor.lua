2752c532641edb03c60a74ffa4eae157e7a7c707
jnwang <jnwangcn@gmail.com>
2025-12-29 00:48:50 +0800

    update: update smearcursor setting.

--- lua/woniulol/plugins/smearcursor.lua
+++ lua/woniulol/plugins/smearcursor.lua
@@ -2,8 +2,16 @@ return {
     {
         "sphamba/smear-cursor.nvim",
         opts = {
-            legacy_computing_symbols_support = true, -- Transparent background
-            time_interval = 5,
+            stiffness = 0.8,                      -- 0.6      [0, 1]
+            trailing_stiffness = 0.6,             -- 0.45     [0, 1]
+            stiffness_insert_mode = 0.7,          -- 0.5      [0, 1]
+            trailing_stiffness_insert_mode = 0.7, -- 0.5      [0, 1]
+            damping = 0.95,                       -- 0.85     [0, 1]
+            damping_insert_mode = 0.95,           -- 0.9      [0, 1]
+            distance_stop_animating = 0.5,        -- 0.1      > 0
+            time_interval = 3,
+            legacy_computing_symbols_support = false,
+            transparent_bg_fallback_color = "#f4dbd6",
         },
     },
 }
