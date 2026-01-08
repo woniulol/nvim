80a74a921ffaa7fd3edf7d7660b6d2930b395417
jnwang <jnwangcn@gmail.com>
2026-01-08 18:15:36 +0800

    update: update dashboard config.

--- lua/woniulol/plugins/dashboard.lua
+++ lua/woniulol/plugins/dashboard.lua
@@ -27,10 +27,40 @@ return {
 ⢨⠁⣄⢮⣐⡭⣠⡅⠈⠉⡔⢩⡄⢡⢰⡍⠁⠊⠀⠀⠂⠒⠆⠐⠂⠤⠡
                 ]]
             logo = string.rep("\n", 8) .. logo .. "\n\n"
+
+            local function nvim_version()
+                local v = vim.version()
+                return string.format("v%d.%d.%d", v.major, v.minor, v.patch)
+            end
+
+            local function get_os()
+                local os = jit and jit.os or vim.loop.os_uname().sysname
+                return string.format("%s ", os)
+            end
+
+            local function get_arch()
+                local arch = jit and jit.arch or vim.loop.os_uname().machine
+                return string.format("%s", arch)
+            end
+
             require('dashboard').setup({
                 config = {
+                    shortcut = {
+                        {
+                            desc = "Neovim ",
+                            key = nvim_version(),
+                            action = "",
+                        },
+                        {
+                            desc = get_os(),
+                            key = get_arch(),
+                            action = "",
+                        },
+                    },
                     header = vim.split(logo, "\n"),
-                    footer = { "", "     People live longer, but people love shorter." }
+                    footer = { "", "     People live longer, but people love shorter." },
+                    project = { enable = true, limit = 2 }, -- Somehow this is n+1.
+                    mru = { limit = 8 },
                 }
             })
         end,
--- lua/woniulol/plugins/smearcursor.lua
+++ lua/woniulol/plugins/smearcursor.lua
@@ -5,9 +5,9 @@ return {
             damping = 1,             -- 0.85     [0, 1]
             damping_insert_mode = 1, -- 0.9      [0, 1]
             time_interval = 1,
-            legacy_computing_symbols_support = false,
+            -- legacy_computing_symbols_support = false,
             transparent_bg_fallback_color = "#f4dbd6",
-            distance_stop_animating_vertical_bar = 1,
+            -- distance_stop_animating_vertical_bar = 1,
         },
     },
 }
